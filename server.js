9a9486c0e876e914bdfdcd1b1fbfac28dd7fa3f1
Victor Tirona <vlbtirona@gmail.com>
2025-01-30 01:18:41 +0800

    Triggering re-deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { Pool } = require('pg')
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 require('dotenv').config();
 const supabase = require('./supabaseClient.js')
 
